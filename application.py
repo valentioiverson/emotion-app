f3746be968c0af287cc80ecb666f74b4959a498c
deathtrigger03 <valentioiversontjokro@gmail.com>
2022-06-19 14:42:22 -0400

    third change

--- application.py
+++ application.py
@@ -27,7 +27,7 @@ import streamlit as st
 import emoji 
 
 #Input Data and Data Cleaning
-data = pd.read_csv('../Data Mood Analysis.csv')
+data = pd.read_csv('../emotion-app/Data Mood Analysis.csv')
 
 stop_words = stopwords.words('english')
 cleaned_data = []
